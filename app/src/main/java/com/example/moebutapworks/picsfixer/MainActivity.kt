50658186b085d5215ff0beff3109a2b696feb7ee
Aburasumashi81104 <boostedzombie@gmail.com>
2017-10-04 21:38:37 +0900

    second version

--- app/src/main/java/com/example/moebutapworks/picsfixer/MainActivity.kt
+++ app/src/main/java/com/example/moebutapworks/picsfixer/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.moebutapworks.picsfixer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +14,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d(TAG, "This is a first log!")
+        val button: Button = findViewById(R.id.button) as Button
+        button.setOnClickListener { view ->
+            android.util.Log.d(TAG, "Click!")
+        }
     }
 }
