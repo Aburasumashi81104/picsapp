a4b197c97022f465423e0c2c6226cc3b07b16442
Aburasumashi81104 <boostedzombie@gmail.com>
2017-10-10 21:56:04 +0900

    set button

--- app/src/main/java/com/example/moebutapworks/picsfixer/MainActivity.kt
+++ app/src/main/java/com/example/moebutapworks/picsfixer/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Gallery
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,9 +15,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d(TAG, "This is a first log!")
-        val button: Button = findViewById(R.id.button) as Button
+        val Gallery: Button = findViewById(R.id.Gallery) as Button
         button.setOnClickListener { view ->
             android.util.Log.d(TAG, "Click!")
         }
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "This is a second log!")
+        val Joint: Button = findViewById(R.id.Joint) as Button
+        button.setOnClickListener { view ->
+            android.util.Log.d(TAG, "Click!")
+        }
+    }
+
 }
