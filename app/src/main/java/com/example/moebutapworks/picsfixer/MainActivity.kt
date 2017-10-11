4b9fb65e9982e5035dd7059f48e0895dc7355f44
Aburasumashi81104 <boostedzombie@gmail.com>
2017-10-11 21:52:27 +0900

    enable to generate new window & intent

--- app/src/main/java/com/example/moebutapworks/picsfixer/MainActivity.kt
+++ app/src/main/java/com/example/moebutapworks/picsfixer/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.moebutapworks.picsfixer
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 
 
 class MainActivity : AppCompatActivity() {
 
     val TAG = "MainActivity"
+    private var image: ImageView? =null
+    private var fileUri: Uri? = null
+    private var editedFileUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,27 +24,55 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "Slelect Theme!!")
         val theme: Button = findViewById(R.id.theme) as Button
         theme.setOnClickListener { view ->
-            Log.d(TAG, "Click!")
+            Log.d(TAG, "theme button is Clicked!")
         }
 
         Log.d(TAG, "This is a first log!!")
         val gallery: Button = findViewById(R.id.gallery) as Button
-        gallery.setOnClickListener { view ->
-            Log.d(TAG, "Click!")
+        gallery.setOnClickListener{
+            launchGallery()
         }
 
         Log.d(TAG, "This is a second log!!")
         val joint: Button = findViewById(R.id.joint) as Button
-        joint.setOnClickListener { view ->
-            Log.d(TAG, "Click!")
+        joint.setOnClickListener {
+            newSampleActivity()
         }
 
         Log.d(TAG, "This is a first log!")
         val photo: Button = findViewById(R.id.photo) as Button
         photo.setOnClickListener { view ->
-            Log.d(TAG, "Click!")
+            Log.d(TAG, "photo button is Clicked!")
         }
 
     }
 
+    private fun launchGallery(){
+        val intent = Intent()
+        intent.action = Intent.ACTION_PICK
+        intent.data =
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        startActivityForResult(intent,REQUEST_CODE_GALLERY)
+    }
+
+    companion object {
+        val REQUEST_CODE_GALLERY = 30
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK){
+            when(requestCode){
+                REQUEST_CODE_GALLERY -> {
+                    fileUri = data?.data
+                    Log.d(TAG, "gallery fileUri " + fileUri)
+                }
+            }
+        }
+    }
+
+    private fun newSampleActivity() {
+        val intent = Intent()
+        intent.setClass(this, NewSampleActivity::class.java)
+        startActivity(intent)
+    }
 }
---
+++ app/src/main/java/com/example/moebutapworks/picsfixer/NewSampleActivity.kt
@@ -0,0 +1,23 @@
+package com.example.moebutapworks.picsfixer
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class NewSampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_sample)
+    }
+
+
+
+}
+
+
+
+
+/**
+ * Created by MoebutaP on 2017/10/11.
+ */
