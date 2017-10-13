95ab0fa1bdc07b0d54e90996f9c80939472e3f70
Aburasumashi81104 <boostedzombie@gmail.com>
2017-10-14 00:18:44 +0900

    fixed button problem

--- app/src/main/java/com/example/moebutapworks/picsfixer/MainActivity.kt
+++ app/src/main/java/com/example/moebutapworks/picsfixer/MainActivity.kt
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
 
 
